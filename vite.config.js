985b2d23aaf3e39c415cd4e7872159e55d8212a5
thuctapsinh4 <minhhieunguyen2684@gmail.com>
2025-10-03 14:16:52 +0700

    push

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
-  base: command === "build" ? "/HavenAroma/" : "/",
-}));
+  base: "/", // Netlify luôn dùng /
+});
